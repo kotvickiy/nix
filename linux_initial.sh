3e9745c7fc5edc71f5272af8fe18de9b6172b6ae
kotvickiy <87358776+kotvickiy@users.noreply.github.com>
2022-06-02 08:15:18 +0500

    Update linux_initial.sh

--- linux_initial.sh
+++ linux_initial.sh
@@ -48,7 +48,7 @@ ufw allow samba
 (echo "$pass"; echo "$pass") | smbpasswd -s -a "$newuser"
 
 /home/$newuser/nix/install_wg.sh  --unattended /home/$newuser/nix/options.conf
-echo "rp" | pivpn add
+echo "$1" | pivpn add
 echo 1 | pivpn -qr
 
 rm -r /home/$newuser/nix
