9bf71586ad31c5bd0300ec8c199a797ccd40a3a8
kotvickiy <87358776+kotvickiy@users.noreply.github.com>
2022-06-04 23:31:29 +0500

    Update linux_initial.sh

--- linux_initial.sh
+++ linux_initial.sh
@@ -26,9 +26,9 @@ update-locale LANG=ru_RU.UTF-8
 
 apt install samba -y
 mkdir /mnt/sda
-mount -t $4 /dev/sda1 /mnt/sda/
+mount /dev/sda1 /mnt/sda/
 uuid=`blkid | grep sda | cut -d '"' -f 4`
-echo UUID=$uuid /mnt/sda $4 defaults 0 0 >> /etc/fstab
+echo UUID=$uuid /mnt/sda auto defaults 0 0 >> /etc/fstab
 
 echo " workgroup = WORKGROUP
     netbios name = ubuntu
